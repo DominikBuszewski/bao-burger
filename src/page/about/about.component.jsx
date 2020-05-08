b3bdffa2bc2500157b5762082e80423cf33caa3e
Dominik Buszewski <yogiasdasd@gmail.com>
2020-05-08 10:59:18 +0200

    font change, header color change, minor css changes, mobile view bugs fixed

--- src/page/about/about.component.jsx
+++ src/page/about/about.component.jsx
@@ -3,70 +3,69 @@ import "./about.styles.scss";
 import GallerySlider from "../../components/gallery/gallery.component";
 
 const AboutPage = () => (
-  <div className="about">
-    <div className="sections">
-      <section className="main-info">
-        <h2 className="section-title">ABOUT US</h2>
-        <div className="text-wraper">
-          <p>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
-            massa egestas mollis varius; dignissim elementum. Mollis tincidunt
-            mattis hendrerit dolor eros enim, nisi ligula ornare. Hendrerit
-            parturient habitant pharetra rutrum gravida porttitor eros feugiat.
-            Mollis elit sodales taciti duis praesent id. Consequat urna vitae
-            morbi nunc congue assa egestas mollis varius; dignissim elementum.
-            Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula
-            ornare. Hendrerit parturient habitant pharetra rutrum gravida
-            porttitor eros feugiat. Mollis elit sodales taciti duis praesent id.
-            Consequat urna vitae morbi nunc congue.{" "}
-          </p>
-        </div>
-      </section>
+	<div className="about">
+		<div className="sections">
+			<section className="main-info">
+				<h2 className="section-title">ABOUT US</h2>
 
-      <section className="gallery">
-        <h2 className="section-title">GALLERY</h2>
-        <GallerySlider />
-      </section>
+				<p>
+					Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa
+					egestas mollis varius; dignissim elementum. Mollis tincidunt mattis
+					hendrerit dolor eros enim, nisi ligula ornare. Hendrerit parturient
+					habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
+					sodales taciti duis praesent id. Consequat urna vitae morbi nunc
+					congue assa egestas mollis varius; dignissim elementum. Mollis
+					tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
+					Hendrerit parturient habitant pharetra rutrum gravida porttitor eros
+					feugiat. Mollis elit sodales taciti duis praesent id. Consequat urna
+					vitae morbi nunc congue.{" "}
+				</p>
+			</section>
 
-      <section className="workers-info">
-        <h2 className="section-title">MEET YOUR COOK</h2>
-        <div className="worker-container">
-          <div className="worker">
-            <img />
-            <h3 className="worker-name">Lorem Ipsum</h3>
-            <div className="cook-image-container">
-              <img className="cook1" />
-            </div>
-            <p>
-              Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
-              massa egestas mollis varius
-            </p>
-          </div>
-          <div className="worker">
-            <h3 className="worker-name">Ipsum Lorem</h3>
-            <div className="cook-image-container">
-              <img className="cook2" />
-            </div>
-            <img />
-            <p>
-              Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
-              massa egestas mollis varius
-            </p>
-          </div>
-          <div className="worker">
-            <h3 className="worker-name">Lora Ipsem</h3>
-            <div className="cook-image-container">
-              <img className="cook3" />
-            </div>
-            <p>
-              Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
-              massa egestas mollis varius
-            </p>
-          </div>
-        </div>
-      </section>
-    </div>
-  </div>
+			<section className="gallery">
+				<h2 className="section-title">GALLERY</h2>
+				<GallerySlider />
+			</section>
+
+			<section className="workers-info">
+				<h2 className="section-title">MEET YOUR COOK</h2>
+				<div className="worker-container">
+					<div className="worker">
+						<img />
+						<h3 className="worker-name">Lorem Ipsum</h3>
+						<div className="cook-image-container">
+							<img className="cook1" />
+						</div>
+						<p>
+							Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
+							massa egestas mollis varius
+						</p>
+					</div>
+					<div className="worker">
+						<h3 className="worker-name">Ipsum Lorem</h3>
+						<div className="cook-image-container">
+							<img className="cook2" />
+						</div>
+						<img />
+						<p>
+							Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
+							massa egestas mollis varius
+						</p>
+					</div>
+					<div className="worker">
+						<h3 className="worker-name">Lora Ipsem</h3>
+						<div className="cook-image-container">
+							<img className="cook3" />
+						</div>
+						<p>
+							Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
+							massa egestas mollis varius
+						</p>
+					</div>
+				</div>
+			</section>
+		</div>
+	</div>
 );
 
 export default AboutPage;
--- src/page/homepage/homepage.component.jsx
+++ src/page/homepage/homepage.component.jsx
@@ -4,28 +4,28 @@ import burger from "../../assets/burger.png";
 import { Link } from "react-router-dom";
 
 const HomePage = () => (
-  <div className="homepage">
-    <div className="sign">
-      <p>Best burger in town.</p>
-      <h1 className="title">
-        <span>BAO </span>BURGER
-      </h1>
-      <p className="slogan">Contact us to see for yourself.</p>
-      <div className="buttons">
-        <Link to="/About">
-          <button className="to-about navigation-button">LEARN MORE</button>
-        </Link>
-        <Link to="/Menu">
-          <button className="to-menu navigation-button">ORDER</button>
-        </Link>
-      </div>
-    </div>
-    <div className="burger-image">
-      <div className="burger-image-container">
-        <img src={burger} alt="burger picture" className="burger" />
-      </div>
-    </div>
-  </div>
+	<div className="homepage">
+		<div className="sign">
+			<p>Best burger in town.</p>
+			<h1 className="title">
+				<span>BAO </span>BURGER
+			</h1>
+			<p className="slogan">Contact us to see for yourself.</p>
+			<div className="buttons">
+				<Link to="/About">
+					<button className="to-about navigation-button">MORE</button>
+				</Link>
+				<Link to="/Menu">
+					<button className="to-menu navigation-button">ORDER</button>
+				</Link>
+			</div>
+		</div>
+		<div className="burger-image">
+			<div className="burger-image-container">
+				<img src={burger} alt="burger picture" className="burger" />
+			</div>
+		</div>
+	</div>
 );
 
 export default HomePage;
